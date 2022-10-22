c45a6f9f9ce40f930f809e0bd24a262c76ff8703
Sanidhya <sharmasanidhya0504@gmail.com>
2022-10-22 20:24:46 +0530

    started working on grid structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './gridmap/grid/grid.component';
+import { CellComponent } from './gridmap/cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
